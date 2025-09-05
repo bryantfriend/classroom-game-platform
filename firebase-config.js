168524fde8d212228c5dfb75fbb64042275a6e30
bryantfriend <72289778+bryantfriend@users.noreply.github.com>
2025-09-06 00:05:28 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,16 @@
+// In your firebase-config.js file
+
+// Make sure to include the "export" keyword
 export const firebaseConfig = {
-  apiKey: "AIzaSyBkcPon3PsSNzTqNYhBmTbdQWE9Mj6ZRII",
+  apiKey: "AIzaSyBkcPon3PsSNzTqNYhBmTbdQWE9Mj6ZRII", // This is a public key, but be careful not to expose server keys
   authDomain: "classroom-game-platform.firebaseapp.com",
-  databaseURL: "https://classroom-game-platform-default-rtdb.firebaseio.com", // important
+  
+  // --- THIS IS THE CRITICAL LINE YOU ARE MISSING ---
+  // You MUST add this for your Realtime Database to work correctly.
+  databaseURL: "https://classroom-game-platform-default-rtdb.asia-southeast1.firebasedatabase.app",
+  
   projectId: "classroom-game-platform",
-  storageBucket: "classroom-game-platform.appspot.com", // corrected
+  storageBucket: "classroom-game-platform.firebasestorage.app",
   messagingSenderId: "442759682850",
   appId: "1:442759682850:web:7c9635351f73ef4bf6e37b",
   measurementId: "G-SL2V5EZ26C"
