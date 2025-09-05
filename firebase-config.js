2b12170d34704b030a810d2ac6fa2ce15cfcbb96
bryantfriend <72289778+bryantfriend@users.noreply.github.com>
2025-09-05 23:30:13 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBkcPon3PsSNzTqNYhBmTbdQWE9Mj6ZRII",
+  authDomain: "classroom-game-platform.firebaseapp.com",
+  databaseURL: "https://classroom-game-platform-default-rtdb.firebaseio.com", // important
+  projectId: "classroom-game-platform",
+  storageBucket: "classroom-game-platform.appspot.com", // corrected
+  messagingSenderId: "442759682850",
+  appId: "1:442759682850:web:7c9635351f73ef4bf6e37b",
+  measurementId: "G-SL2V5EZ26C"
+};
